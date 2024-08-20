108ea4699ac650f82f0a6adafd52bb9559b3f663
cicero lucas <cicerolucassilva10@gmail.com>
2024-08-19 22:02:04 -0300

    refactor(app): refatorar o código no app.js
    Melhorar a estrutura e a legibilidade do código no arquivo app.js para facilitar a manutenção e futuras implementações.

---
+++ frontend/app/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Rotas from './routes/route';
+import './assets/css/style.css';
+import './assets/css/styleAdmin.css';
+import './assets/css/resposividade.css'
+import 'react-toastify/dist/ReactToastify.css';
+
+const App =() => <Rotas/>;
+
+export default App
